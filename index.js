2a5c9459a9cd1daa02909ef8d16491d1ac968998
Moxited <34715976+Moxited@users.noreply.github.com>
2017-12-25 15:22:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -816,4 +816,15 @@ function handleYTError(err)
     console.log(err.toString());
 }
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

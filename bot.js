b08ee6d6eef3fed6127bb504d49bf042e7b5d9df
Moxited <34715976+Moxited@users.noreply.github.com>
2017-12-25 15:13:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message'', message => {
 });
 
 // THIS MUST BE  THIS WAY
-client.login(process.env.MzkxMzI5Njg1MzczNTgzMzYw.DSKUiA.0bhIZZ3pA9sE98CE6vajsu7mebY);
+client.login(process.env.BOT_TOKEN);
